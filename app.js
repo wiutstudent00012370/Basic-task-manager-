5e1d66c3b0280512a4d6fd4b2819603d96011509
azizaslam <jaxonovaziz41@gmail.com>
2022-03-30 16:05:06 +0500

    minor update

--- app.js
+++ app.js
@@ -1,13 +1,74 @@
+
+//third party libs
+
 const express =  require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
 const app =  express()
+//node libs
+const fs = require('fs')
 
 const PORT = 8000
 
 app.set('view engine', 'pug')
 
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false}))
 
+
+//http://localhost:8000
 app.get('/', (req, res) => {
-    res.render('HOME')
+    fs.readFile('./data/todos.json', (err, data) => {
+        if(err) throw err
+
+        const todos = JSON.parse(data)
+
+        res.render('home', { todos: todos})
+    })
+
+})
+
+app.post('/add', (req, res) => {
+    const formData = req.body
+
+    if (formData.todo.trim() == '') {
+
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+
+            const todos = JSON.parse(data)
+            res.render('home', { error: true, todos: todos })
+        })
+        
+    } else {
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+
+            const todos = JSON.parse(data)
+
+
+            const todo = {
+                id: id(),
+                description: formData.todo,
+                done: false
+            }
+            todos.push(todo)
+
+            fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+                if (err) throw err
+
+                fs.readFile('./data/todos.json', (err, data) => {
+                    if (err) throw err
+
+                    const todos = JSON.parse(data)
+                    res.render('home', { success: true, todos: todos })
+                })
+
+                
+            })
+        })
+
+    }
 })
 
 
@@ -17,3 +78,9 @@ app.listen(PORT, (err) => {
 
     console.log(`This app is rinning on port ${PORT}`)
 })
+
+
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
